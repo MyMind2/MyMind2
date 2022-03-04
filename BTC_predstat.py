cce279ca35a51b6d383637c4d8755d1b07d342b0
MartyM123 <86890275+MartyM123@users.noreply.github.com>
2022-03-04 22:01:01 +0100

    updated

--- BTC_predstat.py
+++ BTC_predstat.py
@@ -26,3 +26,38 @@ prediction_days = 60
 future_days = 1
 
 x_train, y_train = [], []
+
+for i in range(len(scaled_data)):
+  x_train.append(scaled_data[i-prediction_days:i])
+  y_train.append(scaled_data[i:i+prediction_days])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
+
+
+#model configuration
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=prediction_days))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+model.save_weights('./BTC_AI/modelsave')
+
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', test_start, test_end)
+
+for i in range(len(test_data)):
+  x_train.append(test_data[i-prediction_days:i])
+  y_train.append(test_data[i:i+prediction_days])
