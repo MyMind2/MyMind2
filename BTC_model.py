6fc3f7eb7ac64b1c54d6f8ea928470ac8a59ea19
MartyM123 <86890275+MartyM123@users.noreply.github.com>
2022-03-05 18:51:56 +0100

    Update BTC_model.py

--- BTC_model.py
+++ BTC_model.py
@@ -1,1 +1,53 @@
+import numpy as np
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
 
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential
+
+crypto_currency = 'BTC'
+against_currency = 'USD'
+
+start = dt.datetime(2016, 1, 1)
+end = dt.datetime.now()
+
+data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
+
+
+#prepare data
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+future_days = 1
+
+x_train, y_train = [], []
+
+for i in range(len(scaled_data)):
+  x_train.append(scaled_data[i-prediction_days:i])
+  y_train.append(scaled_data[i:i+prediction_days])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
+
+
+#model configuration
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=prediction_days))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+model.save_weights('./BTC_AI/modelsave')
